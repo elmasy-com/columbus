1046eb6d266cb6cf20d9a8e66399f60118e64daa
g0rbe <mail@danielgorbe.com>
2023-09-13 06:39:03 +0200

    feat(server): add cache headers

--- server/server/stat/api.go
+++ server/server/stat/api.go
@@ -2,6 +2,7 @@ package stat
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/elmasy-com/columbus/db"
 	"github.com/gin-gonic/gin"
@@ -16,6 +17,11 @@ func GetApiStat(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
 		return
 	}
+
+	// Cache for 10 minutes
+	c.Header("cache-control", "public, max-age=600")
+	c.Header("expires", time.Now().In(time.UTC).Add(600*time.Second).Format(time.RFC1123))
+	c.Header("vary", "Accept")
 
 	c.JSON(http.StatusOK, s)
 }
